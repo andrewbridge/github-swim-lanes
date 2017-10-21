37b15cacaecf17fb03aec08ae0e927dc7bff9c3c
Andrew Bridge <a.bridge@coinvestor.co.uk>
2017-10-21 17:45:44 +0100

    Stop being lazy and accept json

--- auth.php
+++ auth.php
@@ -7,11 +7,12 @@ $url = "https://github.com/login/oauth/access_token"; // where you want to post 
 curl_setopt($ch, CURLOPT_URL,$url);
 curl_setopt($ch, CURLOPT_POST, true);  // tell curl you want to post something
 curl_setopt($ch, CURLOPT_POSTFIELDS, "client_id=".$clientDeets['clientId']."&client_secret=".$clientDeets['clientSecret']."&code=".$_REQUEST['code']); // define what you want to post
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Accept: application/json']); // Accept json so the response is in json
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // return the output in string format
 $output = curl_exec ($ch); // execute
  
 curl_close ($ch); // close curl handle
  
-$token = str_replace('&token_type=bearer', '', str_replace('access_token=', '', $output));
+$token = json_decode($output, true)['access_token'];
 setcookie("GHSwimLaneToken", $token, time()+3600);
 header( 'Location: '.$clientDeets['clientUrl'] ) ;
