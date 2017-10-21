001f8920261e7569720662b281f4ef4dcfe0cd49
Andrew Bridge <a.bridge@coinvestor.co.uk>
2017-10-21 17:31:22 +0100

    This isn't javascript

--- auth.php
+++ auth.php
@@ -6,7 +6,7 @@ $ch = curl_init();                    // initiate curl
 $url = "https://github.com/login/oauth/access_token"; // where you want to post data
 curl_setopt($ch, CURLOPT_URL,$url);
 curl_setopt($ch, CURLOPT_POST, true);  // tell curl you want to post something
-curl_setopt($ch, CURLOPT_POSTFIELDS, "client_id="+$clientDeets['clientId']+"&client_secret="+$clientDeets['clientSecret']+"&code="+$_REQUEST['code']); // define what you want to post
+curl_setopt($ch, CURLOPT_POSTFIELDS, "client_id=".$clientDeets['clientId']."&client_secret=".$clientDeets['clientSecret']."&code=".$_REQUEST['code']); // define what you want to post
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // return the output in string format
 $output = curl_exec ($ch); // execute
  
